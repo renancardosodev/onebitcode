7980b99c7e70a0be462b9a021b86c0e1ef6ce93b
Renan Cardoso <renancardoso.dev@gmail.com>
2023-12-05 13:44:07 -0300

    Primeiro commit

---
+++ JAVASCRIPT/JS I/ex 01/script.js
@@ -0,0 +1,8 @@
+let primeiroNome = prompt('Seu nome:')
+let sobrenome = prompt('Seu sobrenome:')
+let campoEstudo = prompt('Informe o campo de estudo:')
+let anoNascimento = prompt('Informe o seu ano de nascimento:')
+
+console.log(primeiroNome + " " + sobrenome)
+console.log(campoEstudo)
+console.log(2023 - anoNascimento + " anos")
---
+++ JAVASCRIPT/JS I/ex 01/script2.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt('Primeiro número:'))
+let num2 = Number(prompt('Segundo número:'))
+
+console.log(num1 - num2)
+console.log(num1 + num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
