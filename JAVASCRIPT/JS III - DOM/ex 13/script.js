136fc54bb7d4df5a044857704d41a839e6424f95
Renan Cardoso <renancardoso.dev@gmail.com>
2023-12-28 18:07:18 -0300

    Jogo funcionando ex13 JSIII

--- JAVASCRIPT/JS III - DOM/ex 13/script.js
+++ JAVASCRIPT/JS III - DOM/ex 13/script.js
@@ -1,4 +1,62 @@
-function iniciarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2) {
+function mostrarGanhador(ganhador){
+    console.log(ganhador)
+}
+
+function verficarSeGanhou(arr, a, b, c, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2){
+    if(arr[a].innerText === escolhaPlayer1 && arr[b].innerText === escolhaPlayer1 && arr[c].innerText === escolhaPlayer1) {
+        return namePlayer1
+    } else if(arr[a].innerText === escolhaPlayer2 && arr[b].innerText === escolhaPlayer2 && arr[c].innerText === escolhaPlayer2) {
+        return namePlayer2
+    } else {
+        return false
+    }
+}
+
+function iniciarJogo(currentPlayer, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2){
+    const spanCasa = [...document.querySelectorAll('.spanCasa')]
+    let validacao = false
+    let velha = 0
+    let clicks = 1
+
+    spanCasa.forEach((e,i,arr)=>{        
+            e.addEventListener('click', ()=>{
+                if(e.innerText == ''){
+                    if(clicks%2) {
+                        e.innerText = escolhaPlayer1
+                        currentPlayer.innerText = namePlayer2
+                    }else {
+                        e.innerText = escolhaPlayer2
+                        currentPlayer.innerText = namePlayer1
+                    }
+
+                    validacao = verficarSeGanhou(arr, 0, 1, 2, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 3, 4, 5, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 6, 7, 8, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 0, 4, 8, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 2, 4, 6, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 0, 3, 6, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 1, 4,7, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+                    validacao = verficarSeGanhou(arr, 2, 5, 8, escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+                    if(validacao) return validacao
+
+                    if(e.innerText != '') velha++
+                    if(velha === 9) return 'velha'
+
+                    clicks++
+                }
+            })
+        
+    })
+}
+
+function criarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2) {
     const currentPlayer = document.createElement('section')
     const sectionGame = document.createElement('section')
     const newGame = document.createElement('section')
@@ -8,7 +66,6 @@ function iniciarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2) {
     const spanScoreJogador2 = document.createElement('span')
     const pScoreJogador2 = document.createElement('p')
     const score2 = document.createElement('p')  
-
     const spanScoreJogador1 = document.createElement('span')
     const pScoreJogador1 = document.createElement('p')
     const score1 = document.createElement('p')    
@@ -41,6 +98,11 @@ function iniciarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2) {
     spanScoreJogador2.append(pScoreJogador2,score2)
     sectionGame.append(spanScoreJogador1, tabuleiro, spanScoreJogador2)
     document.querySelector('main').append(currentPlayer, sectionGame, newGame)
+
+
+
+    const ganhador = iniciarJogo(currentPlayer,escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+    mostrarGanhador(ganhador)
 }
 
 function criarPlayers(){
@@ -64,7 +126,7 @@ function criarPlayers(){
 
     if(namePlayer1 != "" && namePlayer2 != "" && btnEscolhaApertado == true) {
         document.getElementById('escolherPlayers').remove()
-        iniciarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
+        criarJogo(escolhaPlayer1, escolhaPlayer2, namePlayer1, namePlayer2)
     }else{
         if(document.getElementById('spanError')){
             document.getElementById('spanError').remove()
