05ac76b575382d27facbfce6d2e528bfb1faae79
Renan Cardoso <renancardoso.dev@gmail.com>
2024-01-09 15:48:46 -0300

    finalizando o EX14 JSIII

--- JAVASCRIPT/JS III - DOM/ex14/script.js
+++ JAVASCRIPT/JS III - DOM/ex14/script.js
@@ -11,21 +11,28 @@ function mediaPonderada(...obj){
     let totalnumeros = 0
     let totalpesos = 0
     obj.forEach((e)=>{
-        totalnumeros += (e.n * e.p)
-        totalpesos += e.p
+        totalnumeros += ((e.n ?? 1) * (e.p ?? 1))
+        totalpesos += e.p ?? 1
     })
     console.log((totalnumeros/totalpesos).toFixed(2))
 }
 
 function mediana(...numeros){
-    const meio = numeros.length/2
+    const meio = Math.floor(numeros.length)/2
     if(numeros.length % 2 === 0){
-        console.log((numeros[(Math.floor(meio)-1)] + numeros[(Math.ceil(meio))])/2)
+        mediaAritmeticaSimples(numeros[meio-1], numeros[meio])
     }else {
         console.log(numeros[Math.floor(meio)])
     }
 }
 
+function moda(...numeros){
+    let qnt = numeros.map(e => [e, numeros.filter(n => n == e).length])
+    qnt.sort((a,b) => b[1] - a[1])
+    console.log(qnt[0][0])
+}
+
 mediaAritmeticaSimples(5,5,5,5,5,5)
 mediaPonderada({n:2,p:7}, {n:5,p:5} ,{n:10,p:2})
-mediana(2,3,8,8,10,5)
+mediana(2,8,10,5)
+moda(2,3,5,6,4,4,8,1,2,2,6,1,8,1)
