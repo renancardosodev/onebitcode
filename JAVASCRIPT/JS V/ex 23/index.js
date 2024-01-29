588bd25ab934e864b9f5da47a12b3bdb5bcf16c0
Renan Cardoso <renancardoso.dev@gmail.com>
2024-01-29 10:47:04 -0300

    Inicio do ex 23 JSVI

---
+++ JAVASCRIPT/JS V/ex 23/index.js
@@ -0,0 +1,13 @@
+function checkEmail(email) {
+
+}
+
+function checkPassword(password) {
+
+}
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+checkEmail(email.value)
+checkPassword(password.value)
