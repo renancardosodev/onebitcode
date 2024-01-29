79c5f188af314e01e73ffeb24c847e37867ce814
Renan Cardoso <renancardoso.dev@gmail.com>
2024-01-29 18:16:34 -0300

    Fim ex23 JSVI

--- JAVASCRIPT/JS V/ex 23/index.js
+++ JAVASCRIPT/JS V/ex 23/index.js
@@ -1,13 +1,51 @@
-function checkEmail(email) {
-
+function validateEmail(email) {
+    if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
+        const err = new Error('Email inválido.')
+        err.input = 'email'
+        throw err
+    }
 }
 
-function checkPassword(password) {
-
+function validatePassword(password) {
+    if (
+        password.length < 8 || 
+        !password.match(/[a-z]/) || 
+        !password.match(/[A-Z]/) || 
+        !password.match(/[0-9]/) ||
+        !password.match(/[^a-zA-Z0-9\s]/)
+    ) {
+        const err = new Error('Senha inválida.')
+        err.input = 'password'
+        throw err
+    }
 }
 
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+function resetFormStyles() {
+    Object.entries(userInputs).forEach(([key, value]) => {
+        value.classList.remove('success', 'error')
+        document.querySelector(`#${key}-error`).textContent = ''
+    })
+}
 
-checkEmail(email.value)
-checkPassword(password.value)
+const userInputs = {}
+
+userInputs.name = document.querySelector('#name')
+userInputs.email = document.querySelector('#email')
+userInputs.password = document.querySelector('#password')
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+    resetFormStyles()
+    try {
+        userInputs.name.classList.add('success')
+        validateEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+        validatePassword(userInputs.password.value)
+        userInputs.password.classList.add('success')
+    } catch (err) {
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#${err.input}-error`).textContent = err.message
+    }
+})
